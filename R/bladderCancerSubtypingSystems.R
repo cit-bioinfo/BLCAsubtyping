51b72ba28f403509f7ca5cdf80ddd56acaf898a6
Aurelie Kamoun <aurelie.kamoun@MAC003.ligue-cancer.net>
2018-11-27 16:26:40 +0100

    Add example dataset

--- R/bladderCancerSubtypingSystems.R
+++ R/bladderCancerSubtypingSystems.R
@@ -1,19 +1,4 @@
-# Rd
-# description >> Assign molecular subtypes to each bladder samples using expression data, according to several classification systems.
-# argument
-# item >> expMat >> Matrix of expression data
-# item >> gpl >> required
-# item >> symbol >> required
-# item >> classification.systems >> A character vector with names of the classification systems to consider among Baylor, UNC, CIT, Lund, MDA and TCGA.
-# value >> A dataframe of samples annotated according to each classification system.
-# author >> Aurelien de Reynies
-# keyword >> methods
-# details >> ...
-# seealso >> ...
-# references >> ...
-# examples >> ...
-# end
-classify <- function(expMat, gpl = NULL, symbol = "Gene.Symbol",  
+classify <- function(expMat, gpl = NULL, symbol = "Gene.Symbol",
                      classification.systems = c("Baylor", "UNC", "CIT", "Lund", "MDA", "TCGA")
                      ){
   
@@ -33,25 +18,25 @@ classify <- function(expMat, gpl = NULL, symbol = "Gene.Symbol",
   
   if("CIT" %in% classification.systems){
       cat("predicting CIT subtypes...")
-      res.class$CIT.subtype <- cit.classify(expMat, Gpl = gpl, Symbol = symbol, classif = "CC")[res.class$ID]
+      res.class$CIT.subtype <- cit.classify(expMat, Gpl = gpl, Symbol = symbol)[res.class$ID]
       cat("...DONE \n")
   }
   
   if("Lund" %in% classification.systems){
       cat("predicting Lund subtypes...")
-      res.class$Lund.subtype <- lund.predict(expMat, Gpl = gpl, Symbol = symbol, classif = "Lund2017")[res.class$ID]
+      res.class$Lund.subtype <- lund.predict(expMat, Gpl = gpl, Symbol = symbol)[res.class$ID]
       cat("...DONE \n")
   }
   
   if("MDA" %in% classification.systems){
     cat("predicting MDA subtypes...")
-    res.class$MDA.subtype <- MDA.predict(expMat, Gpl = gpl, Symbol = symbol, distance.method = "euclid")[res.class$ID]
+    res.class$MDA.subtype <- MDA.predict(expMat, Gpl = gpl, Symbol = symbol)[res.class$ID]
     cat("...DONE \n")
   }
   
   if("TCGA" %in% classification.systems){
     cat("predicting TCGA subtypes...")
-    res.class$TCGA.subtype <- TCGA.predict(expMat, Gpl = gpl, Symbol = symbol, classif = "2017")[res.class$ID]
+    res.class$TCGA.subtype <- TCGA.predict(expMat, Gpl = gpl, Symbol = symbol)[res.class$ID]
     cat("...DONE \n")
   }
 
@@ -128,86 +113,46 @@ chapelHill.predict <- function(Exp, Gpl = NULL, Symbol = "Symbol", nmin = 10, ge
 }
 
 
-cit.classify <- function(Exp, Gpl=NULL, Symbol="Symbol", classif = c("BL", "CC")[1]){
+cit.classify <- function(Exp, Gpl=NULL, Symbol="Symbol"){
   
-  if(classif == "BL") data(CIT.BL_predictor) else if(classif == "CC") data(CIT.CC_predictor) else if(classif == "cs98") data(CIT.cs98_predictor)
+  data(CIT.CC_predictor)
 
   Exp <- cit.probesDataToSymbolData(Exp, Gpl, Symbol, classification.system = "CIT")
   G <- intersect(rownames(pred[[1]]$mean), rownames(Exp))
   D0  <- pred[[1]]$centroidsdata$data
   CL0 <- pred[[1]]$centroidsdata$samples
   
-  if(classif == "BL"){
-      
-      pred. <- cit.centroids(D0[G,CL0[,1]], CL0[, 2], median, dist.meth ="pearson")
-      p <- cit.distToCentroids(Exp[G,], pred.[[1]], dist.meth="pearson", d.isPretreated = T)
-      
-      CIT.subtype <- c("Other", "InvBL")[as.numeric(p[[4]] == "Inv_BL") + 1]
-  }
-  
-  if(classif == "CC"){
-      d.scale <- D0[G, CL0[, 1]]
+  d.scale <- D0[G, CL0[, 1]]
 
-      pred. <- cit.centroids(d.scale, CL0[, 2], rowCentering = NA, dist.meth = "pearson")
-      centro.scale <- pred.$centroids$mean
-      d.pred <- Exp[G, ]
+  pred. <- cit.centroids(d.scale, CL0[, 2], rowCentering = NULL, dist.meth = "pearson")
+  centro.scale <- pred.$centroids$mean
+  d.pred <- Exp[G, ]
       
-      pred.test <- apply(cor(d.pred, centro.scale, use = "complete.obs"), 1, function(x){colnames(centro.scale)[which.max(x)]})
+  pred.test <- apply(cor(d.pred, centro.scale, use = "complete.obs"), 1, function(x){colnames(centro.scale)[which.max(x)]})
       
-      CIT.subtype <- paste("MC", 1:7, sep = "")[match(pred.test, paste("CIT.CC", c(2, 1, 3:7), sep = ""))]
-      
-  }
+  CIT.subtype <- paste("MC", 1:7, sep = "")[match(pred.test, paste("CIT.CC", c(2, 1, 3:7), sep = ""))]
+  
   names(CIT.subtype) <- colnames(Exp)
       
   return(CIT.subtype)
 }
 
 
-lund.predict <- function(Exp, Gpl=NULL, Symbol="Symbol", classif = "Lund2017", rowcentering=TRUE, nmin=100, ExplicitClassName = T){
+lund.predict <- function(Exp, Gpl=NULL, Symbol="Symbol", rowcentering=TRUE, nmin=100, ExplicitClassName = T){
   
   Exp <- cit.probesDataToSymbolData(Exp, Gpl, Symbol, classification.system = "Lund")
   
-  if(classif == "Lund2012"){
+  data(lund2017.centroids)
       
-      data(lund.centroids)
-      
-      G <- intersect(rownames(lund.centroids), rownames(Exp))
-      
-      if(length(G) < nmin) stop("Too few genes to get a prediction.")
-      
-      centroids <- lund.centroids[G, ]
-      Exp <- Exp[G, ]
-      
-      if(rowcentering) Exp <- Exp - apply(Exp, 1, median, "na.rm" = T)
-      
-      tmp <-  as.data.frame(cor(Exp, centroids, use="pair"))
-      names(tmp) <- paste("corTo", names(centroids), sep="")
-      
-      Lund.subtype <-  names(centroids)[apply(tmp, 1, which.max)]
-      
-      if (ExplicitClassName) {
-          Lund.subtype <- c("Uro.A", "Uro.A", "Geno.Unst", "Geno.Unst", "Infilt", "Uro.B", "SCC-l")[match(
-          Lund.subtype,c("MS1a", "MS1b", "MS2a1", "MS2a2", "MS2b1", "MS2b2.1", "MS2b2.2"))]
-      }
-      names(Lund.subtype) <- colnames(Exp)
-
-  }
-  
-  if(classif == "Lund2017"){
-      
-      data(lund2017.centroids)
-      
-      Exp <- t(scale(t(Exp), scale = F))
-      Lund.subtype <- ncc.corr(lund.centroids, Exp)
-      Lund.subtype[which(Lund.subtype %in% c("GU-Inf1", "GU-Inf2"))] <- "GU-Inf"
-      
-  }
+  Exp <- t(scale(t(Exp), scale = F))
+  Lund.subtype <- ncc.corr(lund.centroids, Exp)
+  Lund.subtype[which(Lund.subtype %in% c("GU-Inf1", "GU-Inf2"))] <- "GU-Inf"
   
   return(Lund.subtype)
 }
 
 
-MDA.predict <- function(Exp, Gpl = NULL, Symbol = "Symbol", nmin = 2000, distance.method = c("euclid", "pearson")[2]){
+MDA.predict <- function(Exp, Gpl = NULL, Symbol = "Symbol", nmin = 2000){
   
   if(is.null(Gpl)) {
     Gpl <- as.data.frame(cbind("Probe.ID"= rownames(Exp), "Symbol"= rownames(Exp)), stringsAsFactors=F, row.names=rownames(Exp))
@@ -243,15 +188,8 @@ MDA.predict <- function(Exp, Gpl = NULL, Symbol = "Symbol", nmin = 2000, distanc
   d2 <- d2[G, ] - apply(d2[G, ], 1, median, "na.rm" = T)
   d <- cbind(d1, d2)
   
-  if(distance.method == "euclid"){
-    tmp <- as.matrix(dist(t(d)))[colnames(d1), colnames(d2)]
-    cl <- mda.training$clin[apply(tmp, 2, function(z) colnames(d1)[which.min(z)]), "Cluster"]
-  } else if (distance.method == "pearson"){
-    tmp <- cor(d1, d2, use = "complete.obs", method = "pearson")
-    cl <- mda.training$clin[apply(tmp, 2, function(z) colnames(d1)[which.max(z)]), "Cluster"]
-    #clusters <- names(table(mda.training$clin$Cluster))
-    #cl <- clusters[unlist(apply(aggregate(tmp, by = list(mda.training$clin[, "Cluster"]), median)[, -1], 2, which.max))]
-  }
+  tmp <- as.matrix(dist(t(d)))[colnames(d1), colnames(d2)]
+  cl <- mda.training$clin[apply(tmp, 2, function(z) colnames(d1)[which.min(z)]), "Cluster"]
   
   MDA.subtype <- setNames(cl, colnames(d2))
 
@@ -261,9 +199,9 @@ MDA.predict <- function(Exp, Gpl = NULL, Symbol = "Symbol", nmin = 2000, distanc
 
 
 
-TCGA.predict <- function(Exp, Gpl=NULL, Symbol="Symbol", classif = c("2017", "2013")[1]){
+TCGA.predict <- function(Exp, Gpl=NULL, Symbol="Symbol"){
   
-  if(classif == "2017") data(tcga2017.centroids) else data(tcga.centroids)
+  data(tcga2017.centroids)
   
   Exp <- cit.probesDataToSymbolData(Exp,Gpl,Symbol,classification.system="TCGA")
   G <- intersect(rownames(tcga.centroids),rownames(Exp))
