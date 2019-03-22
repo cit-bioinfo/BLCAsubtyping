ee92503659742837fe9d5a521fbba000a7fb4c3e
Aurelie Kamoun <aurelie.kamoun@MAC003.ligue-cancer.net>
2019-03-22 14:57:26 +0100

    minor fix

--- R/bladderCancerSubtypingSystems.R
+++ R/bladderCancerSubtypingSystems.R
@@ -3,7 +3,7 @@ classify <- function(expMat, gpl, symbol = "Gene.Symbol",
                      ){
   
   gpl[, symbol] <- as.character(gpl[, symbol])
-  if (length(is.na(gpl[, symbol]))) > 0 gpl <- gpl[-is.na(gpl[, symbol]), ]
+  if (length(is.na(gpl[, symbol])) > 0) gpl <- gpl[-is.na(gpl[, symbol]), ]
   
   res.class <- data.frame(ID = colnames(expMat), stringsAsFactors = F)
   
